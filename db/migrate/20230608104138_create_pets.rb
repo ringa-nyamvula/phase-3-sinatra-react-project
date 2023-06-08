3ed2d9a41970420037a6c237829582e9db2c997e
ringa-nyamvula <nyamvularinga@gmail.com>
2023-06-08 13:51:10 +0300

    Update on Tables

--- db/migrate/20230608104138_create_pets.rb
+++ db/migrate/20230608104138_create_pets.rb
@@ -1,10 +1,10 @@
-class CreatePets < ActiveRecord::Migration[7.0]
+class CreatePets < ActiveRecord::Migration[6.1]
   def change
     create_table :pets do |t|
-    t.string :name
-      t.string :breed
-      t.string :image_url
-      t.integer :user_id 
+      t.string :name
+        t.string :breed
+        t.string :image_url
+        t.integer :user_id 
     end
   end
 end
